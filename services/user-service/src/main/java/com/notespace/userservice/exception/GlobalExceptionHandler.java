de8561e10e87d1efa5551d5997d7e1a572d7ab6b
Teterskyi Vladyslav <matganzovka90@gmail.com>
2026-09-13 15:31:37 +0300

    refactor: rewrite global exception handler with new fields

--- services/user-service/src/main/java/com/notespace/userservice/exception/GlobalExceptionHandler.java
+++ services/user-service/src/main/java/com/notespace/userservice/exception/GlobalExceptionHandler.java
@@ -1,28 +1,48 @@
 package com.notespace.userservice.exception;
 
 import com.notespace.userservice.dto.error.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<ErrorResponse> handleUserAlreadyExists(
-            UserAlreadyExistsException ex
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidation(
+            MethodArgumentNotValidException ex,
+            HttpServletRequest request
     ) {
-        ErrorResponse error = new ErrorResponse(
-                HttpStatus.CONFLICT.value(),
-                ex.getMessage(),
-                LocalDateTime.now()
+
+        Map<String, String> fields = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        DefaultMessageSourceResolvable::getDefaultMessage,
+                        (existing, replacement) -> existing
+                ));
+
+        ErrorResponse response = new ErrorResponse(
+                Instant.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "VALIDATION_ERROR",
+                "Request validation failed",
+                request.getRequestURI(),
+                fields
         );
 
         return ResponseEntity
-                .status(HttpStatus.CONFLICT)
-                .body(error);
+                .badRequest()
+                .body(response);
     }
 }
