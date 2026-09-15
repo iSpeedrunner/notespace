31aebb6ad6b62a6abe25ba733e75e576c15d9e88
Teterskyi Vladyslav <matganzovka90@gmail.com>
2026-09-16 00:47:34 +0300

    feat: create two tests

--- services/user-service/src/test/java/com/notespace/userservice/service/AuthServiceTest.java
+++ services/user-service/src/test/java/com/notespace/userservice/service/AuthServiceTest.java
@@ -1,9 +1,10 @@
 package com.notespace.userservice.service;
 
-import com.notespace.userservice.dto.auth.RegisterRequest;
-import com.notespace.userservice.dto.auth.UserResponse;
+import com.notespace.userservice.dto.auth.*;
+import com.notespace.userservice.entity.RefreshToken;
 import com.notespace.userservice.entity.User;
 import com.notespace.userservice.exception.EmailAlreadyExistsException;
+import com.notespace.userservice.exception.InvalidCredentialsException;
 import com.notespace.userservice.exception.UsernameAlreadyExistsException;
 import com.notespace.userservice.repository.AuthRepository;
 import com.notespace.userservice.security.JwtService;
@@ -12,15 +13,18 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -30,6 +34,15 @@ public class AuthServiceTest {
 
     @Mock
     private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private JwtService jwtService;
+
+    @Mock
+    private RefreshTokenService refreshTokenService;
+
+    @Mock
+    private AuthenticationManager authenticationManager;
 
     @InjectMocks
     private AuthService authService;
@@ -133,4 +146,76 @@ public class AuthServiceTest {
         verify(passwordEncoder).encode(request.password());
     }
 
+
+    @Test
+    void login_ShouldReturnLoginResponse_WhenCredentialsAreValid() {
+        String email = "test@gmail.com";
+        String password = "password123";
+        UUID id = UUID.randomUUID();
+
+        LoginRequest req = new LoginRequest(email, password);
+
+        User user = User.builder()
+                .id(id)
+                .email(email)
+                .build();
+
+        RefreshTokenResult refreshTokenResult = new RefreshTokenResult(
+                mock(RefreshToken.class),
+                "refresh-token-raw"
+        );
+
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenReturn(mock(Authentication.class));
+
+        when(authRepository.findByEmail(email))
+                .thenReturn(Optional.of(user));
+
+        when(jwtService.generateToken(email))
+                .thenReturn("access-token");
+
+        when(refreshTokenService.createRefreshToken(id))
+                .thenReturn(refreshTokenResult);
+
+        LoginResponse response = authService.login(req);
+
+        assertNotNull(response);
+        assertEquals("access-token", response.accessToken());
+        assertEquals("refresh-token-raw", response.refreshToken());
+
+        verify(authenticationManager).authenticate(any());
+        verify(authRepository).findByEmail(email);
+        verify(jwtService).generateToken(email);
+        verify(refreshTokenService).createRefreshToken(id);
+    }
+
+    @Test
+    void login_shouldThrowInvalidCredentialsException_whenPasswordIsIncorrect() {
+        LoginRequest request = new LoginRequest(
+                "test@gmail.com",
+                "wrong_password"
+        );
+
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class)))
+                .thenThrow(new BadCredentialsException("Bad credentials"));
+
+        assertThrows(
+                InvalidCredentialsException.class,
+                () -> authService.login(request)
+        );
+
+        verify(authenticationManager).authenticate(
+                any(UsernamePasswordAuthenticationToken.class)
+        );
+        verifyNoInteractions(
+                authRepository,
+                jwtService,
+                refreshTokenService
+        );
+    }
+
+    @Test
+    void login_shouldNormalizeEmail_beforeAuthentication() {
+
+    }
 }
